52c042eea9bcde7dbf08bad3fb9384ed9efee797
Hong Jiang <hjiang@jeos.localdomain>
2008-10-07 21:50:16 -0700

    added map

--- linqxx.h
+++ linqxx.h
@@ -1,31 +1,78 @@
 #ifndef LINQXX_H_
 #define LINQXX_H_
 
+#include <vector>
+
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+
 namespace linqxx {
+
+using std::vector;
+using boost::shared_ptr;
+using boost::function;
 
 // T is usually an STL container.
 template<typename T>
 class DataSet {
   public:
-    DataSet(const T&);
+    DataSet(shared_ptr<T>);
     unsigned int count();
+    DataSet<T> where(function<bool (typename T::value_type)> pred);
+
+    // TODO: There is probably some way to get rid of the hard-coded
+    // vector and instead use the same container type as T.
+    template<typename S>
+    DataSet<vector<S> > select(function<S (typename T::value_type)> sel);
+
+    // Map is an alias of select.
+    template<typename S>
+    DataSet<vector<S> > map(function<S (typename T::value_type)> sel) {
+        select<S>(sel);
+    }
+
   private:
-    T container_;
+    shared_ptr<T> container_;
 };
 
 template<typename T>
-DataSet<T>::DataSet(const T& container) : container_(container) {}
+DataSet<T>::DataSet(shared_ptr<T> container) : container_(container) {}
 
 template<typename T>
 unsigned int DataSet<T>::count() {
-    return container_.size();
+    return container_->size();
 }
 
 template<typename T>
-DataSet<T> from(const T& source) {
+DataSet<T> DataSet<T>::where(function<bool (typename T::value_type)> pred) {
+    shared_ptr<T> new_container(new T());
+    for (typename T::const_iterator i = container_->begin();
+         i != container_->end(); ++i) {
+        if (pred(*i)) {
+            new_container->push_back(*i);
+        }
+    }
+    return DataSet<T>(new_container);
+}
+
+template<typename T>
+template<typename S>
+DataSet<vector<S> > DataSet<T>::select(function<S (typename T::value_type)> sel) {
+    shared_ptr<vector<S> > new_container(new vector<S>());
+    for (typename T::const_iterator i = container_->begin();
+         i != container_->end(); ++i) {
+        new_container->push_back(sel(*i));
+    }
+    return DataSet<vector<S> >(new_container);
+}
+
+template<typename T>
+DataSet<T> from(shared_ptr<T> source) {
     return DataSet<T>(source);
 }
 
 }  // namespace linqxx
+
+#define LX_ATTR(A) &_1 ->* &(A)
 
 #endif  // LINQXX_H_
--- linqxx_test.cc
+++ linqxx_test.cc
@@ -2,10 +2,16 @@
 #include <string>
 #include <vector>
 
+#include <boost/lambda/lambda.hpp>
+#include <gtest/gtest.h>
+
 #include "linqxx.h"
 
+namespace linqxx {
+namespace {
+
 using namespace std;
-using namespace linqxx;
+using boost::lambda::_1;
 
 struct Person {
     string name;
@@ -13,10 +19,33 @@ struct Person {
     Person(const string& aName, int anAge) : name(aName), age(anAge) {}
 };
 
-int main() {
-    vector<Person> guests;
-    guests.push_back(Person("John", 32));
-    guests.push_back(Person("Mike", 28));
-    guests.push_back(Person("Eliz", 27));
-    cout << from(guests).count() << endl;
+class LinqxxTest : public testing::Test {
+  protected:
+    virtual void SetUp() {
+        guests_.reset(new vector<Person>());
+        guests_->push_back(Person("John", 32));
+        guests_->push_back(Person("Mike", 28));
+        guests_->push_back(Person("Eliz", 27));
+    }
+
+    shared_ptr<vector<Person> > guests_;
+};
+
+TEST_F(LinqxxTest, Count) {
+    EXPECT_EQ(3, from(guests_).count());
 }
+
+TEST_F(LinqxxTest, Where) {
+    EXPECT_EQ(1, from(guests_).where(&_1 ->* &Person::age > 30).count());
+}
+
+TEST_F(LinqxxTest, Select) {
+    EXPECT_EQ(3, from(guests_).select<int>(&_1 ->* &Person::age).count());
+}
+
+TEST_F(LinqxxTest, Map) {
+    EXPECT_EQ(3, from(guests_).map<int>(&_1 ->* &Person::age).count());
+}
+
+}  // anonymous namespace
+}  // namespace linqxx
